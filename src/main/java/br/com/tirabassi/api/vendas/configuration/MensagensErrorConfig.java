1288d7cef7179bd7b7df0df332c2c5758c95e662
Vinicius Tirabassi <vinicvp@hotmail.com>
2021-03-31 11:04:36 -0300

    add swagger

--- src/main/java/br/com/tirabassi/api/vendas/configuration/MensagensErrorConfig.java
+++ src/main/java/br/com/tirabassi/api/vendas/configuration/MensagensErrorConfig.java
@@ -2,13 +2,14 @@ package br.com.tirabassi.api.vendas.configuration;
 
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 import java.util.Locale;
 
-@Component
+@Configuration
 public class MensagensErrorConfig {
 
     @Bean
